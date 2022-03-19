35082f9e1cf4f1815f304e6cc2942eb4dee71d09
Mark Gauffin <markgauffin@gmail.com>
2022-03-19 05:55:41 -0500

    test(Amer to Brit): favorite

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -2,7 +2,15 @@ const chai = require('chai');
 const assert = chai.assert;
 
 const Translator = require('../components/translator.js');
+const translator = new Translator();
 
 suite('Unit Tests', () => {
-
+    suite('American to British', () => {
+        test('favorite', () => {
+            assert.equal(
+                translator.americanToBritish('Mangoes are my favorite fruit'),
+                'Mangoes are my favourite fruit.'
+            );
+        });
+    });
 });
