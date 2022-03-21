55a832a25af2c1ecf5e75e241fa058ed496b6dd1
Mark Gauffin <markgauffin@gmail.com>
2022-03-21 04:32:16 -0500

    feat: translate american to british

--- components/translator.js
+++ components/translator.js
@@ -1,10 +1,29 @@
 const americanOnly = require('./american-only.js');
 const americanToBritishSpelling = require('./american-to-british-spelling.js');
-const americanToBritishTitles = require("./american-to-british-titles.js")
-const britishOnly = require('./british-only.js')
+const americanToBritishTitles = require('./american-to-british-titles.js');
+const britishOnly = require('./british-only.js');
 
 class Translator {
+    americanToBritish(string) {
+        // Loop over each word in string
+        const dictionaries = [
+            americanOnly,
+            americanToBritishSpelling,
+            americanToBritishTitles,
+        ];
 
+        for (const dictionary of dictionaries) {
+            for (const property in dictionary) {
+                const english = property;
+                const british = dictionary[property];
+                string = string.replace(english, british);
+            }
+        }
+        return string;
+    }
 }
 
+const translator = new Translator();
+const result = translator.americanToBritish('Mangoes are my favorite fruit');
+console.log(result);
 module.exports = Translator;
