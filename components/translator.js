7fdff4240d41e22d0b6aa67b05e7aa688e95108f
Mark Gauffin <markgauffin@gmail.com>
2022-03-22 07:58:32 -0500

    feat: return correct title american to british

--- components/translator.js
+++ components/translator.js
@@ -13,17 +13,31 @@ class Translator {
 
         for (const dictionary of dictionaries) {
             for (const property in dictionary) {
-                // String that needs to be replaced cannot have letters before or after
-                const english = new RegExp(`\\b${property}\\b`, 'gi');
                 const british = dictionary[property];
+                const britishCap = british[0].toUpperCase() + british.slice(1);
+
+                // String that needs to be replaced cannot have letters before or after
+                let english = new RegExp(`\\b${property}\\b`, 'g');
+                let englishCap = new RegExp(`\\b${property}\\b`, 'ig');
+
+                // If dictionary is titles, ok to end in a period
+                if (dictionary === americanToBritishTitles) {
+                    english = new RegExp(`\\b${property}(?!\S)`, 'g');
+                    englishCap = new RegExp(`\\b${property}(?!\S)`, 'ig');
+                }
+
                 string = string.replace(english, british);
+                // Check for capitalized letter
+                string = string.replace(englishCap, britishCap);
             }
         }
         return string;
     }
 }
 
-const translator = new Translator();
-const result = translator.americanToBritish('Mangoes are my favorite fruit');
-console.log(result);
+// const translator = new Translator();
+// const result = translator.americanToBritish(
+//     'No Mr. Bond, I expect you to die.'
+// );
+// console.log(result);
 module.exports = Translator;
