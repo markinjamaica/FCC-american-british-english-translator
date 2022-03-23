00dadd3f4533e2a563d886a4faff6a1fe9715749
Mark Gauffin <markgauffin@gmail.com>
2022-03-23 05:54:18 -0500

    feat: translate british to american

--- components/translator.js
+++ components/translator.js
@@ -40,9 +40,82 @@ class Translator {
         }
         return string;
     }
+
+    britishToAmerican(string) {
+        const dictionaries = [
+            britishOnly,
+            americanToBritishSpelling,
+            americanToBritishTitles,
+        ];
+
+        for (const dictionary of dictionaries) {
+            for (const property in dictionary) {
+                const timeRegex = /(?<=\d{1,2}).(?=\d{2})/g;
+
+                // if britishOnly dictionary, british is property, and english is property value
+                if (dictionary === britishOnly) {
+                    const english = dictionary[property];
+                    const englishCap =
+                        english[0].toUpperCase() + english.slice(1);
+
+                    // String that needs to be replaced cannot have letters before or after
+                    let british = new RegExp(`\\b${property}\\b`, 'g');
+                    let britishCap = new RegExp(`\\b${property}\\b`, 'ig');
+
+                    // Replace english words & phrases with british
+                    string = string.replace(british, english);
+                    // Check for capitalized letter
+                    string = string.replace(britishCap, englishCap);
+
+                    // British is property value, and English is property
+                } else {
+                    const english = property;
+                    const englishCap =
+                        english[0].toUpperCase() + english.slice(1);
+
+                    // String that needs to be replaced cannot have letters before or after
+                    let british = new RegExp(
+                        `\\b${dictionary[property]}\\b`,
+                        'g'
+                    );
+                    let britishCap = new RegExp(
+                        `\\b${dictionary[property]}\\b`,
+                        'ig'
+                    );
+
+                    // If dictionary is titles, ok to end in a period
+                    if (dictionary === americanToBritishTitles) {
+                        british = new RegExp(
+                            `\\b${dictionary[property]}(?!\S)`,
+                            'g'
+                        );
+                        britishCap = new RegExp(
+                            `\\b${dictionary[property]}(?!\S)`,
+                            'ig'
+                        );
+                    }
+
+                    // Replace english words & phrases with british
+                    string = string.replace(british, english);
+                    // Check for capitalized letter
+                    string = string.replace(britishCap, englishCap);
+                    // Check for time
+                    if (timeRegex.test(string)) {
+                        string = string.replace(
+                            string.match(timeRegex)[0],
+                            ':'
+                        );
+                    }
+                }
+            }
+        }
+        return string;
+    }
 }
 
 // const translator = new Translator();
-// const result = translator.americanToBritish('Lunch is at 12:15 today.');
+// const result = translator.britishToAmerican(
+//     'We watched the footie match for a while.'
+// );
 // console.log(result);
 module.exports = Translator;
