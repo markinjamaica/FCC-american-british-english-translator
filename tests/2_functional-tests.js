5d36a61af290382f12ee4c203a32359403cf51d6
Mark Gauffin <markgauffin@gmail.com>
2022-03-25 06:45:25 -0500

    test: translation with text and locale fields

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -8,5 +8,20 @@ chai.use(chaiHttp);
 let Translator = require('../components/translator.js');
 
 suite('Functional Tests', () => {
-
+    test('Translation with text and locale fields', (done) => {
+        chai.request(server)
+            .post('/api/translate')
+            .send({
+                text: 'Paracetamol takes up to an hour to work.',
+                locale: 'british-to-american',
+            })
+            .end((err, res) => {
+                assert.equal(res.status, 200);
+                assert.equal(
+                    res.body.translation,
+                    '<span class="highlight">Tylenol</span> takes up to an hour to work.'
+                );
+                done();
+            });
+    });
 });
