411fe9c4126895079b02f156c893b1b4fb181dee
Mark Gauffin <markgauffin@gmail.com>
2022-03-25 07:22:59 -0500

    feat: return error for empty text

--- routes/api.js
+++ routes/api.js
@@ -6,9 +6,13 @@ module.exports = function (app) {
     const translator = new Translator();
 
     app.route('/api/translate').post((req, res) => {
+        if (req.body.text === '') {
+            return res.json({ error: 'No text to translate' });
+        }
         if (!req.body.text || !req.body.locale) {
             return res.json({ error: 'Required field(s) missing' });
         }
+
         const text = req.body.text;
         const locale = req.body.locale;
 
