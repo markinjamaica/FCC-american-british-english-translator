a6ce6153da927ceacab9509d64e6cfb23567f52a
Mark Gauffin <markgauffin@gmail.com>
2022-03-25 06:46:14 -0500

    feat: return translation on POST req

--- routes/api.js
+++ routes/api.js
@@ -3,11 +3,29 @@
 const Translator = require('../components/translator.js');
 
 module.exports = function (app) {
-  
-  const translator = new Translator();
+    const translator = new Translator();
 
-  app.route('/api/translate')
-    .post((req, res) => {
-      
+    app.route('/api/translate').post((req, res) => {
+        const text = req.body.text;
+        const locale = req.body.locale;
+
+        let translationObj;
+
+        if (locale === 'american-to-british') {
+            translationObj = translator.americanToBritish(text);
+        } else {
+            translationObj = translator.britishToAmerican(text);
+        }
+
+        let translation = translationObj.string;
+
+        for (const translatedWords of translationObj.highlight) {
+            translation = translation.replace(
+                translatedWords,
+                `<span class="highlight">${translatedWords}</span>`
+            );
+        }
+
+        res.json({ translation: translation });
     });
 };
